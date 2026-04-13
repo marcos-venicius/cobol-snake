959c0078b54b42c6886d643c6f75ad0992236817
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-04-13 07:47:41 -0300

    rename variables

--- main.cbl
+++ main.cbl
@@ -15,31 +15,31 @@
          05 MOVEMENT-KEY PIC X(1).
 
        WORKING-STORAGE SECTION.
-       01 GRID-WIDTH             PIC 9(2)  VALUE 10.
-       01 GRID-HEIGHT            PIC 9(2)  VALUE 10.
-       01 PLAYER-DIR-X           PIC S9(1) VALUE 1.
-       01 PLAYER-DIR-Y           PIC S9(1) VALUE 0.
-       01 NEXT-PLAYER-X          PIC S9(1) VALUE 0.
-       01 NEXT-PLAYER-Y          PIC S9(1) VALUE 0.
-       01 FRUIT-X                PIC S9(2) VALUE 5.
-       01 FRUIT-Y                PIC S9(2) VALUE 5.
-       01 FRUIT-LIFETIME         PIC S9(3) VALUE 0.
-       01 L-ROW                  PIC 9(2)  VALUE 0.
-       01 L-COL                  PIC 9(2)  VALUE 0.
-       01 POINTS                 PIC 9(3)  VALUE 0.
-       01 RENDER-CONTROL         PIC 9(8)  VALUE 0.
-       01 FRAMES-BETWEEN-RENDER  PIC 9(8)  VALUE 15000000.
-       01 SNAKE-SIZE             PIC 9(3)  VALUE 1.
-       01 SNAKE.
+       01 WS-GRID-WIDTH             PIC 9(2)  VALUE 10.
+       01 WS-GRID-HEIGHT            PIC 9(2)  VALUE 10.
+       01 WS-PLAYER-DIR-X           PIC S9(1) VALUE 1.
+       01 WS-PLAYER-DIR-Y           PIC S9(1) VALUE 0.
+       01 WS-NEXT-WS-PLAYER-X       PIC S9(1) VALUE 0.
+       01 WS-NEXT-WS-PLAYER-Y       PIC S9(1) VALUE 0.
+       01 WS-FRUIT-X                PIC S9(2) VALUE 5.
+       01 WS-FRUIT-Y                PIC S9(2) VALUE 5.
+       01 WS-FRUIT-LIFETIME         PIC S9(3) VALUE 0.
+       01 WS-L-ROW                  PIC 9(2)  VALUE 0.
+       01 WS-L-COL                  PIC 9(2)  VALUE 0.
+       01 WS-POINTS                 PIC 9(3)  VALUE 0.
+       01 WS-RENDER-CONTROL         PIC 9(8)  VALUE 0.
+       01 WS-FRAMES-BETWEEN-RENDER  PIC 9(8)  VALUE 15000000.
+       01 WS-SNAKE-SIZE             PIC 9(3)  VALUE 1.
+       01 WS-SNAKE.
          05 SNAKE-CELL PIC 9(4) OCCURS 100 TIMES
            INDEXED BY SNAKE-IDX.
-       01 PLAYER-X               PIC 9(2)  VALUE 0.
-       01 PLAYER-Y               PIC 9(2)  VALUE 0.
-       01 TAIL-X                 PIC 9(2)  VALUE 0.
-       01 TAIL-Y                 PIC 9(2)  VALUE 0.
-       01 RENDERED               PIC 9(1)  VALUE 0.
-       01 START-TIMESTAMP        PIC 9(14).
-       01 START-DATETIME         PIC X(21).
+       01 WS-PLAYER-X               PIC 9(2)  VALUE 0.
+       01 WS-PLAYER-Y               PIC 9(2)  VALUE 0.
+       01 WS-TAIL-X                 PIC 9(2)  VALUE 0.
+       01 WS-TAIL-Y                 PIC 9(2)  VALUE 0.
+       01 WS-RENDERED               PIC 9(1)  VALUE 0.
+       01 WS-START-TIMESTAMP        PIC 9(14).
+       01 WS-START-DATETIME         PIC X(21).
 
        PROCEDURE DIVISION.
        0001-MAIN-LOGIC.
@@ -47,23 +47,26 @@
 
            PERFORM 0001-CURRENT-TIME.
 
-           COMPUTE PLAYER-X = 
-           FUNCTION INTEGER 
-           ( (GRID-WIDTH - 1) * FUNCTION RANDOM(START-TIMESTAMP) ) + 1.
+           COMPUTE WS-PLAYER-X =
+           FUNCTION INTEGER
+           ((WS-GRID-WIDTH - 1) *
+           FUNCTION RANDOM(WS-START-TIMESTAMP)) + 1.
 
-           COMPUTE PLAYER-Y = 
-           FUNCTION INTEGER 
-           ( (GRID-HEIGHT - 1) * FUNCTION RANDOM(START-TIMESTAMP) ) + 1.
+           COMPUTE WS-PLAYER-Y =
+           FUNCTION INTEGER
+           ( (WS-GRID-HEIGHT - 1) *
+           FUNCTION RANDOM(WS-START-TIMESTAMP) ) + 1.
 
-           COMPUTE SNAKE-CELL(1) = (PLAYER-X * 16) + PLAYER-Y.
+           COMPUTE SNAKE-CELL(1) = (WS-PLAYER-X * 16) + WS-PLAYER-Y.
 
            PERFORM UNTIL 1 < 0
-             IF RENDER-CONTROL = 0
+             IF WS-RENDER-CONTROL = 0
       * Linux specific
                CALL "SYSTEM" USING "clear"
 
-               IF SNAKE-SIZE >= (GRID-WIDTH * GRID-HEIGHT)
-                 DISPLAY "You won everything with " POINTS " points!!!"
+               IF WS-SNAKE-SIZE >= (WS-GRID-WIDTH * WS-GRID-HEIGHT)
+                 DISPLAY "You won everything with " WS-POINTS
+                                                  " points!!!"
                  MOVE 0 TO RETURN-CODE
                  STOP RUN
                END-IF
@@ -74,45 +77,46 @@
                PERFORM 0001-GAME-LOOP
              END-IF
 
-             IF RENDER-CONTROL = FRAMES-BETWEEN-RENDER
-               MOVE 0 TO RENDER-CONTROL
+             IF WS-RENDER-CONTROL = WS-FRAMES-BETWEEN-RENDER
+               MOVE 0 TO WS-RENDER-CONTROL
              ELSE
-               ADD 1 TO RENDER-CONTROL
+               ADD 1 TO WS-RENDER-CONTROL
              END-IF
            END-PERFORM.
 
            STOP RUN.
 
        0001-GAME-LOOP.
-           PERFORM VARYING L-ROW FROM 0 BY 1 UNTIL L-ROW >= GRID-WIDTH
-             PERFORM VARYING L-COL
-               FROM 0 BY 1 UNTIL L-COL >= GRID-HEIGHT
+           PERFORM VARYING WS-L-ROW FROM 0 BY 1
+             UNTIL WS-L-ROW >= WS-GRID-WIDTH
+             PERFORM VARYING WS-L-COL
+               FROM 0 BY 1 UNTIL WS-L-COL >= WS-GRID-HEIGHT
 
-               MOVE 0 TO RENDERED
+               MOVE 0 TO WS-RENDERED
 
                DISPLAY SPACES WITH NO ADVANCING
 
-               PERFORM VARYING SNAKE-IDX FROM 1 BY 1 
-                 UNTIL SNAKE-IDX > SNAKE-SIZE
+               PERFORM VARYING SNAKE-IDX FROM 1 BY 1
+                 UNTIL SNAKE-IDX > WS-SNAKE-SIZE
 
-                 COMPUTE TAIL-X = SNAKE-CELL(SNAKE-IDX) / 16
-                 COMPUTE TAIL-Y = FUNCTION 
+                 COMPUTE WS-TAIL-X = SNAKE-CELL(SNAKE-IDX) / 16
+                 COMPUTE WS-TAIL-Y = FUNCTION
                    MOD(SNAKE-CELL(SNAKE-IDX), 16)
 
-                 IF TAIL-X = L-COL AND TAIL-Y = L-ROW
+                 IF WS-TAIL-X = WS-L-COL AND WS-TAIL-Y = WS-L-ROW
                    IF SNAKE-IDX = 1
                      DISPLAY "O" WITH NO ADVANCING
                    ELSE
                      DISPLAY "o" WITH NO ADVANCING
                    END-IF
-                   MOVE 1 TO RENDERED
+                   MOVE 1 TO WS-RENDERED
                    EXIT PERFORM
                  END-IF
                END-PERFORM
 
-               IF RENDERED = 0
-                 IF FRUIT-X <> -1 OR FRUIT-Y <> -1
-                   IF L-ROW = FRUIT-Y AND L-COL = FRUIT-X
+               IF WS-RENDERED = 0
+                 IF WS-FRUIT-X <> -1 OR WS-FRUIT-Y <> -1
+                   IF WS-L-ROW = WS-FRUIT-Y AND WS-L-COL = WS-FRUIT-X
                      DISPLAY "+" WITH NO ADVANCING
                    ELSE
                      DISPLAY "." WITH NO ADVANCING
@@ -122,8 +126,8 @@
                  END-IF
                END-IF
 
-               IF PLAYER-X = FRUIT-X AND PLAYER-Y = FRUIT-Y
-                 ADD 1 TO POINTS
+               IF WS-PLAYER-X = WS-FRUIT-X AND WS-PLAYER-Y = WS-FRUIT-Y
+                 ADD 1 TO WS-POINTS
                  PERFORM 0001-SPAWN-FRUIT
                  PERFORM 0001-INCREASE-SNAKE-BODY
                END-IF
@@ -131,14 +135,14 @@
              DISPLAY SPACES
            END-PERFORM.
 
-           IF FRUIT-LIFETIME <= 0
+           IF WS-FRUIT-LIFETIME <= 0
              PERFORM 0001-SPAWN-FRUIT
            END-IF.
 
-           ADD -1 TO FRUIT-LIFETIME.
+           ADD -1 TO WS-FRUIT-LIFETIME.
 
            DISPLAY " ".
-           DISPLAY "POINTS: " POINTS.
+           DISPLAY "WS-POINTS: " WS-POINTS.
 
        0001-MOVEMENT-INPUT-LOGIC.
            OPEN INPUT KEY-INPUT-FILE.
@@ -148,85 +152,90 @@
            CLOSE KEY-INPUT-FILE.
 
            IF MOVEMENT-KEY = 'k'
-             MOVE -1 TO PLAYER-DIR-Y
-             MOVE 0  TO PLAYER-DIR-X
+             MOVE -1 TO WS-PLAYER-DIR-Y
+             MOVE 0  TO WS-PLAYER-DIR-X
            END-IF.
 
            IF MOVEMENT-KEY = 'l'
-             MOVE 0 TO PLAYER-DIR-Y
-             MOVE 1 TO PLAYER-DIR-X
+             MOVE 0 TO WS-PLAYER-DIR-Y
+             MOVE 1 TO WS-PLAYER-DIR-X
            END-IF.
 
            IF MOVEMENT-KEY = 'j'
-             MOVE 1 TO PLAYER-DIR-Y
-             MOVE 0 TO PLAYER-DIR-X
+             MOVE 1 TO WS-PLAYER-DIR-Y
+             MOVE 0 TO WS-PLAYER-DIR-X
            END-IF.
 
            IF MOVEMENT-KEY = 'h'
-             MOVE 0  TO PLAYER-DIR-Y
-             MOVE -1 TO PLAYER-DIR-X
+             MOVE 0  TO WS-PLAYER-DIR-Y
+             MOVE -1 TO WS-PLAYER-DIR-X
            END-IF.
 
        0001-AUTO-MOVEMENT-LOGIC.
-           COMPUTE NEXT-PLAYER-X = PLAYER-X + PLAYER-DIR-X.
-           COMPUTE NEXT-PLAYER-Y = PLAYER-Y + PLAYER-DIR-Y.
+           COMPUTE WS-NEXT-WS-PLAYER-X = WS-PLAYER-X + WS-PLAYER-DIR-X.
+           COMPUTE WS-NEXT-WS-PLAYER-Y = WS-PLAYER-Y + WS-PLAYER-DIR-Y.
 
-           IF NEXT-PLAYER-X >= GRID-WIDTH
-             MOVE 0 TO NEXT-PLAYER-X
+           IF WS-NEXT-WS-PLAYER-X >= WS-GRID-WIDTH
+             MOVE 0 TO WS-NEXT-WS-PLAYER-X
            END-IF.
 
-           IF NEXT-PLAYER-X < 0
-             COMPUTE NEXT-PLAYER-X = GRID-WIDTH - 1
+           IF WS-NEXT-WS-PLAYER-X < 0
+             COMPUTE WS-NEXT-WS-PLAYER-X = WS-GRID-WIDTH - 1
            END-IF.
 
-           IF NEXT-PLAYER-Y >= GRID-HEIGHT
-             MOVE 0 TO NEXT-PLAYER-Y
+           IF WS-NEXT-WS-PLAYER-Y >= WS-GRID-HEIGHT
+             MOVE 0 TO WS-NEXT-WS-PLAYER-Y
            END-IF.
 
-           IF NEXT-PLAYER-Y < 0
-             COMPUTE NEXT-PLAYER-Y = GRID-HEIGHT - 1
+           IF WS-NEXT-WS-PLAYER-Y < 0
+             COMPUTE WS-NEXT-WS-PLAYER-Y = WS-GRID-HEIGHT - 1
            END-IF.
 
-           MOVE NEXT-PLAYER-X TO PLAYER-X.
-           MOVE NEXT-PLAYER-Y TO PLAYER-Y.
+           MOVE WS-NEXT-WS-PLAYER-X TO WS-PLAYER-X.
+           MOVE WS-NEXT-WS-PLAYER-Y TO WS-PLAYER-Y.
 
        0001-UPDATE-SNAKE-POSITIONS-LOGIC.
-           IF SNAKE-SIZE > 1
-             PERFORM VARYING SNAKE-IDX FROM SNAKE-SIZE BY -1 
+           IF WS-SNAKE-SIZE > 1
+             PERFORM VARYING SNAKE-IDX FROM WS-SNAKE-SIZE BY -1
                UNTIL SNAKE-IDX <= 1
 
                MOVE SNAKE-CELL(SNAKE-IDX - 1) TO SNAKE-CELL(SNAKE-IDX)
              END-PERFORM
            END-IF.
 
-           COMPUTE SNAKE-CELL(1) = (PLAYER-X * 16) + PLAYER-Y.
+           COMPUTE SNAKE-CELL(1) = (WS-PLAYER-X * 16) + WS-PLAYER-Y.
 
        0001-INCREASE-SNAKE-BODY.
-           COMPUTE TAIL-X = SNAKE-CELL(SNAKE-SIZE) / 16.
-           COMPUTE TAIL-Y = FUNCTION MOD(SNAKE-CELL(SNAKE-SIZE), 16)
+           COMPUTE WS-TAIL-X = SNAKE-CELL(WS-SNAKE-SIZE) / 16.
+           COMPUTE WS-TAIL-Y =
+             FUNCTION MOD(SNAKE-CELL(WS-SNAKE-SIZE), 16)
 
-           ADD PLAYER-DIR-X TO TAIL-X.
-           ADD PLAYER-DIR-Y TO TAIL-Y.
+           ADD WS-PLAYER-DIR-X TO WS-TAIL-X.
+           ADD WS-PLAYER-DIR-Y TO WS-TAIL-Y.
 
-           ADD 1 TO SNAKE-SIZE.
+           ADD 1 TO WS-SNAKE-SIZE.
 
-           COMPUTE SNAKE-CELL(SNAKE-SIZE) = (TAIL-X * 16) + TAIL-Y.
+           COMPUTE SNAKE-CELL(WS-SNAKE-SIZE) =
+             (WS-TAIL-X * 16) + WS-TAIL-Y.
 
        0001-SPAWN-FRUIT.
            PERFORM 0001-CURRENT-TIME.
 
-           COMPUTE FRUIT-X = 
-           FUNCTION INTEGER 
-           ( (GRID-WIDTH - 1) * FUNCTION RANDOM(START-TIMESTAMP) ) + 1.
+           COMPUTE WS-FRUIT-X =
+           FUNCTION INTEGER
+           ((WS-GRID-WIDTH - 1) *
+             FUNCTION RANDOM(WS-START-TIMESTAMP)) + 1.
 
-           COMPUTE FRUIT-Y = 
-           FUNCTION INTEGER 
-           ( (GRID-HEIGHT - 1) * FUNCTION RANDOM(START-TIMESTAMP) ) + 1.
+           COMPUTE WS-FRUIT-Y =
+           FUNCTION INTEGER
+           ((WS-GRID-HEIGHT - 1) *
+             FUNCTION RANDOM(WS-START-TIMESTAMP)) + 1.
 
-           COMPUTE PLAYER-X = SNAKE-CELL(1) / 16.
-           COMPUTE PLAYER-Y = FUNCTION MOD(SNAKE-CELL(1), 16).
-           COMPUTE FRUIT-LIFETIME = GRID-WIDTH * GRID-HEIGHT * 0.5.
+           COMPUTE WS-PLAYER-X = SNAKE-CELL(1) / 16.
+           COMPUTE WS-PLAYER-Y = FUNCTION MOD(SNAKE-CELL(1), 16).
+           COMPUTE WS-FRUIT-LIFETIME =
+             WS-GRID-WIDTH * WS-GRID-HEIGHT * 0.5.
 
        0001-CURRENT-TIME.
-           MOVE FUNCTION CURRENT-DATE TO START-DATETIME.
-           MOVE START-DATETIME(1:14) TO START-TIMESTAMP.
+           MOVE FUNCTION CURRENT-DATE TO WS-START-DATETIME.
+           MOVE WS-START-DATETIME(1:14) TO WS-START-TIMESTAMP.
